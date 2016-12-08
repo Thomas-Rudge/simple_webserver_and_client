554a52629ff22c11e1c2a56a16e13cffdb1d5620
Thomas-Rudge <thomas.rudge85@gmail.com>
2016-12-08 11:23:00 +0000

    Working version
    The client can now issue get requests, and the server makes valid responses. Index file can be gotten.

--- client/my_client.rb
+++ client/my_client.rb
@@ -1,18 +1,19 @@
 require 'net/http'
 
 def client_browser(host, path)
-  http = Net::HTTP.new(host)          # Create a connection
-  headers, body = http.get(path)      # Request the file
+  http = Net::HTTP.new(*host)          # Create a connection
+  resource = http.get(path)      # Request the file
 
-  if headers.code == "200"            # Check the status code
-    print body
+  if resource.code == "200"            # Check the status code
+    print resource.body
   else
-    puts "#{headers.code} #{headers.message}"
+    puts "#{resource.code} #{resource.message}"
   end
 end
 
-if __FILE__ == $0
-  host = 'localhost:2000'
+
+if __FILE__ == $0 || true
+  host = ['localhost', 2000]
   path = '/index.html'
 
   client_browser(host, path)
--- server/my_server.rb
+++ server/my_server.rb
@@ -2,37 +2,59 @@ require 'socket'
 
 module MessageTasks
   def break_apart(request)
+    # Just in case the server recieves other fields (host), or a newline, we first split on
+    # newline, and then break apart the first line by spaces and return it.
     request = request.gsub("\r", "").split("\n")
     request[0] = request[0].split(" ")
 
-    request
+    request[0]
   end
 
   def check_request(request)
     response = nil
     puts "REQ: #{request}"
     case
-    when request[0].lengh != 3
+    when request.length != 3
       response = "HTTP/1.1 400 Bad Request\r\n"
-    when !["HTTP/1.1", "HTTP/1.0"].include? request[2].upcase
+    when !(["HTTP/1.1", "HTTP/1.0"].include? request[2].upcase)
       response = "HTTP/1.1 505 HTTP Version not supported\r\n"
-    when request[2].upcase == "HTTP/1.1"
-      cnt = 0
-      request.each { |field| (field.downcase.start_with? "host:") ? cnt+=1 : nil }
-      if cnt == 0
-        response = "HTTP/1.1 400 Bad Request\r\n"
-      end
     end
 
     response
   end
 
-  def format_response(resource, header)
-    if resource.nil?
-      response = "HTTP/1.1 404 Not Found"
-    else
-      response = "HTTP/1.1 200 OK\r\n#{gen_datetime}\r\nContent-Type:text/plain\r\nContent-Length:#{resource.length}\r\n"
-      response = header ? response : "#{response}\r\n#{resource}\r\n"
+  def format_response(resource, type, http)
+    case
+    when resource.nil?
+      response = "#{http} #{type=="GET" ? "404" : "400"} Not Found"
+    when (["GET", "HEAD"].include? type)
+      response = "#{http} 200 OK\r\n#{gen_datetime}\r\nContent-Type:text/plain\r\nContent-Length:#{resource.length}\r\n"
+      response = type=="HEAD" ? response : "#{response}\r\n#{resource}\r\n"
+    end
+  end
+
+  def gen_datetime
+    Time.now.gmtime.strftime("%a, %d %b %Y %T GMT")
+  end
+end
+
+
+class SimpleServer
+  def initialize(port)
+    @server = TCPServer.open(port)
+  end
+
+  def start
+    loop {
+      client  = @server.accept
+      request = client.gets
+      handler = RequestHandler.new(request)
+
+      response = handler.process_request
+
+      client.puts response
+      client.close
+    }
   end
 end
 
@@ -49,33 +71,31 @@ class RequestHandler
     response = check_request(@request)
     return response unless response.nil?
 
-    response = get_response(@request)
-
+    response = get_response
+    puts "RESP1: #{response}"
+    response = format_response(response, @request[0], @request[2])
+    puts "RESP2: #{response}"
     response
   end
 
-  def get_response(request)
+  def get_response
     response = nil
-    case request[0]
+    case @request[0]
     when "HEAD"
-      response = get(request[1], true)
+      response = get(true)
     when "GET"
-      response = get(request[1], false)
+      response = get(false)
     when "PUT"
       put
     else
-      response = "#{request[2]} 501 Not Implemented\r\n"
+      response = "#{@request[2]} 501 Not Implemented\r\n"
     end
 
     response
   end
 
-  def gen_datetime
-    Time.now.gmtime.strftime("%a, %d %b %Y %T GMT")
-  end
-
-  def get(resource, header)
-    resource = Dir.pwd.concat(resource)
+  def get(header)
+    resource = Dir.pwd.concat(@request[1])
     if File.exist?(resource)
       resource = File.open(resource, "r") { |file| file.read }
       resource = resource.length if header
@@ -91,28 +111,8 @@ class RequestHandler
   end
 end
 
-
-class SimpleServer
-  def initialize(port)
-    @server = TCPServer.open(port)
-  end
-
-  def start(server)
-    loop {
-      client  = server.accept
-      request = client.gets
-      handler = RequestHandler(request)
-
-      response = handler.process_request
-
-      client.puts response
-      client.close
-    }
-  end
-end
-
 # Only start the server if toplevel
-if __FILE__ == $0
+if __FILE__ == $0 || true
   server = SimpleServer.new(2000)
   server.start
 end
